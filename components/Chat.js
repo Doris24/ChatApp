38c096d8bde408c345e0777c3c72d07d9ea32880
Doris24 <tinabara@gmx.net>
2022-04-20 01:49:09 +0200

    5.3

--- components/Chat.js
+++ components/Chat.js
@@ -4,6 +4,10 @@ import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from 'react-na
 //Gifted Chat
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
+//import firebase
+const firebase = require('firebase');
+require('firebase/firestore');
+
 export default class Start extends React.Component {
 
 	constructor(props) {
@@ -11,8 +15,33 @@ export default class Start extends React.Component {
 
 		this.state = {
 			massages: [],
+			uid: 0,
+			user: {
+				_id: '',
+				name: '',
+				avatar: '',
+			},
+
 		};
+
+		// Your web app's Firebase configuration
+		const firebaseConfig = {
+			apiKey: "AIzaSyB8yTyRo8lNEE1LALUqOST2WVIxx0EbMmg",
+			authDomain: "chatapp-bf5d4.firebaseapp.com",
+			projectId: "chatapp-bf5d4",
+			storageBucket: "chatapp-bf5d4.appspot.com",
+			messagingSenderId: "386452457126",
+			appId: "1:386452457126:web:9115b95b7e148081e3d667"
+		};
+
+		if (!firebase.apps.length) {
+			firebase.initializeApp(firebaseConfig);
+		}
+		// reference the database
+		this.referenceChatMessages = firebase.firestore().collection('messages');
+
 	}
+
 
 	//called when user navigates to chatscreen
 	componentDidMount() {
@@ -22,28 +51,86 @@ export default class Start extends React.Component {
 		// set name as title of the chat
 		this.props.navigation.setOptions({ title: name });
 
-		this.setState({
-			messages: [
-				{
-					_id: 1,
-					text: 'Hello ' + name,
-					createdAt: new Date(),
-					user: {
-						_id: 2,
-						name: 'React Native',
-						avatar: 'https://placeimg.com/140/140/any',
-					},
+		this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+			if (!user) {
+				firebase.auth().signInAnonymously();
+			}
+			this.setState({
+				uid: user.uid,
+				messages: [],
+				user: {
+					_id: user.uid,
+					name: name,
+					avatar: 'https://placeimg.com/140/140/any',
 				},
-				// System message
-				{
-					_id: 2,
-					text: name + ' has entered the chat',
-					createdAt: new Date(),
-					system: true,
-				},
-			],
-		})
+			});
+			this.unsubscribe = this.referenceChatMessages
+				.orderBy("createdAt", "desc")
+				.onSnapshot(this.onCollectionUpdate);
+		});
+
+		// this.setState({
+		// 	messages: [
+		// 		{
+		// 			_id: user.uid,
+		// 			messages: [],
+		// 			// text: 'Hello ' + name,
+		// 			// createdAt: new Date(),
+		// 			user: {
+		// 				_id: user.uid,
+		// 				name: 'name',
+		// 				avatar: 'https://placeimg.com/140/140/any',
+		// 			},
+		// 		},
+		// 		// System message
+		// 		{
+		// 			_id: 2,
+		// 			text: name + ' has entered the chat',
+		// 			createdAt: new Date(),
+		// 			system: true,
+		// 		},
+		// 	],
+		// })
 	}
+
+	// retrieve the current data from the collection 
+	// with querySnapshot and store it in state messages
+	onCollectionUpdate = (querySnapshot) => {
+		const messages = [];
+		// go through each document
+		querySnapshot.forEach((doc) => {
+			// get the QueryDocumentSnapshot's data
+			let data = doc.data();
+			messages.push({
+				_id: data._id,
+				text: data.text,
+				createdAt: data.createdAt.toDate(),
+				user: {
+					_id: data.user._id,
+					name: data.user.name,
+					avatar: data.user.avatar,
+				},
+			});
+		});
+		this.setState({
+			messages: messages,
+		});
+	};
+
+	addMessages() {
+		const message = this.state.messages[0];
+
+		this.referenceChatMessages.add({
+			_id: message._id,
+			text: message.text || "",
+			createdAt: message.createdAt,
+			user: this.state.user,
+		});
+	}
+
+	// 
+
+
 
 	// called when a user sends a message
 	// the message a user has just sent gets appended to the 
@@ -51,7 +138,12 @@ export default class Start extends React.Component {
 	onSend(messages = []) {
 		this.setState(previousState => ({
 			messages: GiftedChat.append(previousState.messages, messages),
-		}))
+		}),
+			() => {
+				this.addMessages();
+			}
+
+		);
 	}
 
 	// change Color of the right speech bubbles
@@ -66,6 +158,13 @@ export default class Start extends React.Component {
 				}}
 			/>
 		)
+	}
+
+	componentWillUnmount() {
+		//stop authentication
+		this.authUnsubscribe();
+		//stop listening to changes
+		this.unsubscribe();
 	}
 
 	render() {
@@ -84,7 +183,9 @@ export default class Start extends React.Component {
 					messages={this.state.messages}
 					onSend={messages => this.onSend(messages)}
 					user={{
-						_id: 1,
+						_id: this.state.user._id,
+						name: this.state.name,
+						avatar: this.state.user.avatar,
 					}}
 				/>
 				{/* if OS is Android, then wrap 'view' in 'KeyboardAvoidingView', else do nothing */}
